59a7bd6e9f9d385e1dfc36a7e522bc88da4d9a0e
Tabunov Egor <Vonubat@gmail.com>
2023-01-17 18:15:20 +0300

    refactor: change naming (app -> server) + small fixes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { server } from './app';
+import { server } from './server';
 import { getPort } from './utils';
 
 const PORT = getPort();
 
-server.listen(PORT, () => {
-  console.log(`server started on port: ${PORT}`);
+server.listen(PORT, (): void => {
+  console.log(`Server started on port: ${PORT}`);
 });
--- src/utils/getPort.ts
+++ src/utils/getPort.ts
@@ -5,7 +5,10 @@ export const getPort = (): number => {
   const PORT: string | undefined = process.env.PORT;
 
   if (!PORT) {
-    console.log(`.env doesn't have PORT constant. PORT switched to deafult value (4000)`);
+    console.log(`
+    .env doesn't have PORT constant.
+    PORT switched to deafult value (4000).
+    `);
     return DEFAULT_PORT;
   }
 
